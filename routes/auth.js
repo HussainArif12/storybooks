89c87da81ddba8ab5294fdad83a08fe63b3f39c7
Hussain <hussainarifkl@gmail.com>
2020-11-05 23:21:06 +0500

    added changes

--- routes/auth.js
+++ routes/auth.js
@@ -12,5 +12,9 @@ router.get('/google/callback' , passport.authenticate('google',{ failureRedirect
 (req,res)=>{
     res.redirect('/dashboard')
 })
- 
+router.get('/logout',(req,res)=>{
+
+    req.logout();
+    res.redirect('/');
+})
 module.exports = router;
