aec727d05c9f737473b017df9c073a22c3e7fb7c
Hussain <hussain@pop-os.localdomain>
2020-11-17 23:16:55 +0500

    Added errors

--- models/User.js
+++ models/User.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-   googleId : {
-       type : String,
-       required: true 
-   } , 
-   displayName : {
-    type : String,
-    required: true
-} ,
-firstName : {
-    type : String,
-    required: true
-} ,
-lastName : {
-    type : String,
-    required: true
-},
-image : {
-    type : String,
-    
-    required: true
-},
-createdAt:{
-    type: Date,
-    default : Date.now
-}    
-})
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
 
-module.exports = mongoose.model("User",UserSchema ); 
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
-const express= require('express');
+const express = require("express");
 const router = express.Router();
-const {ensureAuth, ensureGuest} = require('../middleware/auth');
-const Story = require('../models/Story');
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Story = require("../models/Story");
+
 //@desc Login/Landing page
 //@page GET /
-
-router.get('/' , ensureGuest, (req,res)=>{
-    res.render('login', {layout: 'login'});
-})
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", { layout: "login" });
+});
 
 //GET to dashboard
-router.get('/dashboard' ,ensureAuth, (req,res)=>{
-    try {
-        const stories = await Story.find()
-    } catch(err){
-        console.log(err);
-    }
-    res.render('dashboard',{name : req.user.firstName});
-}) 
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean(); //documents returned are basic javascript objects
+    res.render("dashboard", { name: req.user.firstName, stories });
+  } catch (err) {
+    console.log(err);
+    res.render('error/500');
+  }
+});
 
 module.exports = router;
